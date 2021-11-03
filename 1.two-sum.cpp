69e96f5c9874fdbb226e437e87f67b0547d247d2
zhouxingfu <xingfu.zhou@zoho.com>
2021-11-03 16:15:10 +0800

    feat(...): Init.

---
+++ 1.two-sum.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+
+// notes: 1. no more than one solution 2. each element will be just used once.
+// 
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
