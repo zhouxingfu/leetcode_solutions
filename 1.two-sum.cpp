1e17369cd17c38da0e8282fefbc54beb41261f37
zhouxingfu <zhouxingfu@sensetime.com>
2022-03-28 12:09:16 +0800

    feat(...): Add 2-Sum Solution.

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -6,12 +6,34 @@
 
 
 // notes: 1. no more than one solution 2. each element will be just used once.
-// 
+// If two restrictions above does not exist, what should we do?
 // @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        vector<int> index(nums.size(), 0);
+        iota(index.begin(), index.end(), 0);
+        std::sort(index.begin(), index.end(), [&nums](int i1, int i2){return nums[i1] < nums[i2];});
+        vector<int> result;
+        for(int i = 0, j = index.size() - 1; i <j;)
+        {
+            if((nums[index[i]] + nums[index[j]]) == target)
+            {
+               result.push_back(index[i]);
+               result.push_back(index[j]);
+               break;
+            }
+            else if ((nums[index[i]] + nums[index[j]]) > target)
+            {
+                --j;
+            }
+            else 
+            {
+                ++i;
+            }
+
+        }
+        return result; 
     }
 };
 // @lc code=end
